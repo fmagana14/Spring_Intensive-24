dcaa3eaa0362b9890a56a6045c2a63b1935b1369
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2024-03-14 22:40:42 -0700

    can now order from newest to oldest & css completed

--- board/app.py
+++ board/app.py
@@ -4,21 +4,34 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-
-
+# Connect to MongoDB
 client = MongoClient('mongodb://localhost:27017/')
 db = client['trip_database']
 all_trips = db['trips']
 
 # Routes
-@app.route('/')
-def index():
+@app.route('/<sort_order>')
+def index(sort_order):
+    # Check if sorting order is specified in the query parameters
+    
     trips = all_trips.find()
-    print(trips)
+    print(sort_order)
+    # Sort the trips based on the specified order
+    # trips.sort("arrival_date", 1)
+
+    if sort_order == 'oldest':
+        trips = all_trips.find().sort('arrival_date', 1)  # Sort oldest to newest
+    elif sort_order == 'newest':
+        trips = all_trips.find().sort('arrival_date', -1)  # Sort newest to oldest
+    else:
+        # Default sorting order (no sorting)
+        trips = all_trips.find()
+    print(trips[0]["arrival_date"], trips[1]["arrival_date"], trips[2]["arrival_date"])
     return render_template('index.html', trips=trips)
 
 @app.route('/add_trip', methods=['POST'])
 def add_trip():
+    # Retrieve trip data from the form
     location = request.form['location']
     arrival_date = request.form['arrival_date']
     departure_date = request.form['departure_date']
@@ -26,7 +39,7 @@ def add_trip():
     children = int(request.form['children'])
     rating = int(request.form['rating'])
 
-
+    # Create a dictionary to store trip data
     trip_data = {
         'location': location,
         'arrival_date': arrival_date,
@@ -35,16 +48,22 @@ def add_trip():
         'children': children,
         'rating': rating,
     }
+
+    # Insert the trip data into the MongoDB collection
     all_trips.insert_one(trip_data)
 
+    # Redirect to the index route after adding the trip
     return redirect(url_for('index'))
 
 @app.route('/delete_trip/<trip_id>', methods=['POST', 'DELETE'])
 def delete_trip(trip_id):
-    if request.method == 'POST' or request.method == 'DELETE':
+    # Check if the request method is either POST or DELETE
+    if request.method in ['POST', 'DELETE']:
+        # Delete the trip from the MongoDB collection using its ObjectId
         all_trips.delete_one({'_id': ObjectId(trip_id)})
+    # Redirect to the index route after deleting the trip
     return redirect(url_for('index'))
 
-
 if __name__ == "__main__":
+    # Run the Flask application
     app.run(host="0.0.0.0", port=8000, debug=True)
